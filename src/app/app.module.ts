98b9cf53c4d44af5f919abc29adf6070c334c04a
Gaurav Joshi <gaurav.joshi@publicissapient.com>
2020-10-26 16:37:52 +0530

    Added components, pipes and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentComponent } from './components/parent/parent.component';
+import { ChildOneComponent } from './components/child-one/child-one.component';
+import { ChildTwoComponent } from './components/child-two/child-two.component';
+import { CustomSearchPipe } from './pipe/custom-search.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChildOneComponent,
+    ChildTwoComponent,
+    CustomSearchPipe
   ],
   imports: [
     BrowserModule,
